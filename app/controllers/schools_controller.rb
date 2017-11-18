c4c22ff20e11c99be4a6ce0152b4787568ca9655
yasuhisa1984 <penmouse275@nifty.com>
2017-11-18 09:45:36 +0000

    add routes and show簡易

--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -1,4 +1,8 @@
 class SchoolsController < ApplicationController
   def index
   end
+
+  def show
+   @comments = Comment.all
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,3 @@
 Rails.application.routes.draw do
-  get 'schools/index'
-
-  get 'users/index'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :schools
 end
