2639a34c8c209652c62b5f23c28d5db723f33333
Ubuntu <javajava552000@yahoo.co.jp>
2018-03-22 03:20:05 +0000

    問い合わせ機能修正

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,8 @@
 class Contact < ApplicationRecord
+  #include ActiveModel::Model #ここでActiveModelを読み込んで、DBと繋がらないモデルとしている
+  attr_accessor :name, :email, :content #これ大事
+
+  validates :name, :presence => {:message => '名前を入力してください'}
+  validates :email, :presence => {:message => 'メールアドレスを入力してください'}
+  validates :content, :presence => {:message => 'お問い合わせ内容を記載して下さい'}
 end
