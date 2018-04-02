a4126cab26607835f8328091c1d913afb3dfa08d
小鍛冶泰久 <penmouse275@shoutanyayasuhisa-no-MacBook-Pro.local>
2018-04-02 14:23:35 +0900

    fix precompile

---
+++ public/assets/twitter/bootstrap_ujs-9af600a22057de56cf7d2d6494d4c0a62c69ac8f3c7f7b850d95b23e81012e6c.js
@@ -0,0 +1,1 @@
+!function(a){a(function(){a("div.bootstrap-modal").modal(),a("div.bootstrap-modal").modal("hide").addClass("fade"),a("a.bootstrap-modal-cancel-button").click(function(o){a(o.target).closest("div.modal").modal("hide")})})}(jQuery);
